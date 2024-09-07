95f4886d8192b894685fb8f11814e77ed9ba25f4
Alexis Alfaro <alexisalfaro505@gmail.com>
2024-09-07 14:23:39 -0600

    Add a new function for printing in yellow, change the way the list is displayed and add the bubble sort algorithm

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -38,19 +38,29 @@ def green_print(text):
     print(f"\033[92m {text}\033[00m")
 
 
+def yellow_print(text):
+    """
+    Function for printing red text.
+    """
+    print(f"\033[93m {text}\033[00m")
+
+
 def print_colored_list(random_list):
     """
     Function for printing a list colored based on wether or not the elements
     are correctly sort
     """
+    final_string = ''
     k = 0
     print('\nList iteration:')
     while k < len(random_list):
         if random_list[k -1] <= random_list[k] or k == 0:
-            green_print(random_list[k])
+            final_string += f'\033[92m{random_list[k]}\033[00m '
         else:
-            red_print(random_list[k])
+            final_string += f'\033[91m{random_list[k]}\033[00m '
         k += 1
+
+    print(final_string)
 
 
 def insertion_sort(random_list):
@@ -69,13 +79,38 @@ def insertion_sort(random_list):
         print_colored_list(random_list)
 
 
+def bubble_sort(random_list):
+    """
+    Function for programming a sorting algorithm.
+    """
+    swaps = 1
+    while swaps > 0:
+        i = 0
+        print_colored_list(random_list)
+        swaps = 0
+        while i < len(random_list)-1:
+            if random_list[i] > random_list[i + 1]:
+                # print(f'{random_list[i]} is greater than {random_list[i+1]}')
+                aux = random_list[i]
+                random_list[i] = random_list[i + 1]
+                random_list[i + 1] = aux
+                swaps += 1
+
+            i += 1
+            print_colored_list(random_list)
+
+        yellow_print(f'number of swaps: {swaps}')
+
+
+
 def main():
     """
     Where the magic happens!
     """
-    random_list = generate_random_list(10)
-    # random_list = [72, 21, 19, 44, 48, 77, 53, 98, 46, 63]
-    insertion_sort(random_list)
+    # random_list = generate_random_list(10)
+    random_list = [72, 21, 19, 44, 48, 77, 53, 98, 46, 63]
+    # insertion_sort(random_list)
+    bubble_sort(random_list)
 
 
 if __name__ == '__main__':
