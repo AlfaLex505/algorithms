ab75b40ec32f8dded2fdcb600717c98935d153da
Alexis Alfaro <alexisalfaro505@gmail.com>
2024-09-07 13:40:08 -0600

    Add the insertion sort algorithm

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -38,14 +38,36 @@ def green_print(text):
     print(f"\033[92m {text}\033[00m")
 
 
+def insertion_sort(random_list):
+    """
+    Function for programming a sorting algorithm.
+    """
+    for i in range(1, len(random_list)):
+        key = random_list[i]
+        j = i - 1
+
+        while j >= 0 and key < random_list[j]:
+            random_list[j + 1] = random_list[j]
+            j -= 1
+        random_list[j + 1] = key
+
+        k = 0
+        print('\nList iteration:')
+        while k < len(random_list):
+            if random_list[k -1] <= random_list[k] or k == 0:
+                green_print(random_list[k])
+            else:
+                red_print(random_list[k])
+            k += 1
+
+
 def main():
     """
     Where the magic happens!
     """
-    # random_list = generate_random_list(10)
-    random_list = [72, 21, 19, 44, 48, 77, 53, 98, 46, 63]
-    green_print('Hello in color!!!')
-    # insertion_sort(random_list)
+    random_list = generate_random_list(10)
+    # random_list = [72, 21, 19, 44, 48, 77, 53, 98, 46, 63]
+    insertion_sort(random_list)
 
 
 if __name__ == '__main__':
